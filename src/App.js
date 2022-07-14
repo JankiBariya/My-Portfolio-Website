84d6eebd1acabe24282dad5f6086c4fd9f28c647
Janki Bariya <janki.190410107005@gmail.com>
2022-07-14 08:20:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Hello I'm Janki.
+       <div align="center">
+<img width="100%" height = "250px" src="https://raw.githubusercontent.com/onimur/.github/master/.resources/git-header.svg" alt="cover" />
+</div>
+
+<h1> Hello Fellow Developers! <img src = "https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" width = "45"/> </h1>
+<p align='center'></p>
+
+<div size='20px'> My name is Janki Bariya. Thank You for taking the time to view my Website :smile: 
+</div>
+
+<h2> About Me </h2>
+
+{/* <img width="55%" align="right" alt="Github" src="" /> */}
+This site is under deployment. Please visit back soon to see the completed site :)
+
+
+       
+</header>
+    </div>
+  );
+}
+
+export default App;
